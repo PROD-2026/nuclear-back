5a8c01faa6c7b2c436e7d0c9539e1fab1f059aa5
MrOlegTitovDev <MrOlegTitov@yandex.ru>
2026-03-07 15:33:19 +0300

    fix: envs

--- src/infrastructure/adapters/config.py
+++ src/infrastructure/adapters/config.py
@@ -4,5 +4,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
+    debug: bool = False
+
+    uploads_base_path: str
+    projects_base_path: str = "/tmp"
+
+    mongo_uri: str
+    database_name: str
+
     celery_broker_uri: str
     celery_result_backend: str
